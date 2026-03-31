b21c355b3a7884bb141128599a73f1f53ed241ae
Mickredset <aivliev782@gmail.com>
2026-03-31 22:23:16 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+rooms = [1, 2, 3]
+print(rooms)
+rooms_1 = "Комната напротив входа"
+print(rooms_1)
+rooms_2 = "Комната справа от душевой"
+print(rooms_2)
+rooms_3 = "Комната слева от душевой"
+print(rooms_3)
+rooms_1_user = ["Елена, Денис"]
+rooms_2_user = ["Ивлиев"]
+rooms_3_user = ["Денис"]
+# rooms_3_user = ["Денис, Елена"]
---
+++ main.py
@@ -0,0 +1,63 @@
+import pickle
+import os
+import sys
+from config import *
+temp = 0
+print(rooms_1_user)
+print(rooms_2_user)
+print(rooms_3_user)
+while True:
+    cmd = input("main: ")
+    if cmd == "save":
+        print(rooms)
+        with open('data.plk', 'wb') as file:
+            pickle.dump(rooms, file)
+            pickle.dump(rooms_1, file)
+            pickle.dump(rooms_2, file)
+            pickle.dump(rooms_3, file)
+            pickle.dump(rooms_1_user, file)
+            pickle.dump(rooms_2_user, file)
+            pickle.dump(rooms_3_user, file)
+            print(rooms)
+    if cmd == "load":
+        with open('data.plk', 'rb') as file:
+            rooms = pickle.load(file)
+            rooms_1 = pickle.load(file)
+            rooms_2 = pickle.load(file)
+            rooms_3 = pickle.load(file)
+            rooms_1_user = pickle.load(file)
+            rooms_2_user = pickle.load(file)
+            rooms_3_user = pickle.load(file)
+            print(rooms)
+            print(rooms_1_user)
+            print(rooms_2_user)
+            print(rooms_3_user)
+# rooms user add
+    if cmd == "add rooms_1_user":
+        temp = input("add rooms_1_user: ")
+        rooms_1_user.append(temp)
+        temp = 0
+        print(rooms_1_user)
+    if cmd == "add rooms_2_user":
+        temp = input("add rooms_2_user: ")
+        rooms_2_user.append(temp)
+        temp = 0
+        print(rooms_2_user)
+    if cmd == "add rooms_3_user":
+        temp = input("add rooms_3_user: ")
+        rooms_3_user.append(temp)
+        temp = 0
+        print(rooms_3_user)
+# rooms user del
+    if cmd == "del rooms_1_user":
+        rooms_1_user.pop()
+        print(rooms_1_user)
+    if cmd == "del rooms_2_user":
+        rooms_2_user.pop()
+        print(rooms_2_user)
+    if cmd == "del rooms_3_user":
+        rooms_3_user.pop()
+        print(rooms_3_user)
+# reset
+    if cmd == "reset":
+        os.remove("data.plk")
